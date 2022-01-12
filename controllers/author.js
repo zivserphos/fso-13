d7b660e55dbc956a5e4424415bce40d14f57ed60
zivserphos <zivfromisrael@gmail.com>
2022-01-12 14:14:39 +0200

    Finish part b with bonus at 3.16

--- controllers/author.js
+++ controllers/author.js
@@ -1,11 +1,17 @@
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 const sequelize = require('sequelize');
+// const { Op } = sequelize;
 const { SECRET } = require('../util/config');
 
 const { Blog, User } = require('../models/index');
 
 router.get('/', async (req, res) => {
+  // const order = [];
+
+  // if (req.query.order) {
+  //   order.push(['likes', req.query.order]);
+  // }
   try {
     const authors = await Blog.findAll({
       attributes: [
@@ -13,6 +19,7 @@ router.get('/', async (req, res) => {
         [sequelize.fn('COUNT', sequelize.col('title')), 'articles'],
         [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
       ],
+      order: [[sequelize.fn('SUM', sequelize.col('likes')), 'DESC']],
       group: 'author',
     });
     res.send(authors);
